5a6d83d2bc9df406be52a4942ff69ce5bb5dbf78
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-30 20:11:28 +0530

    Create arithmetic.hpp

---
+++ arithmetic.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//creating Arithmtic class
+class Arithmetic {
+  int x;
+  int y;
+
+  public:
+  void input(int x, int y);
+
+  int add();
+  int sub();
+  int mul();
+  int div();
+};
